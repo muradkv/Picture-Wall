17268f16da135c0132b36014b6a3b94f0a196409
murad <muradkv@gmail.com>
2019-05-28 22:28:51 +0300

    Add UI: UIImageView and UILabel

---
+++ Picture Wall/PictureViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  PictureViewCell.swift
+//  Picture Wall
+//
+//  Created by murad on 28/05/2019.
+//  Copyright © 2019 murad. All rights reserved.
+//
+
+import UIKit
+
+class PictureViewCell: UITableViewCell {
+
+    @IBOutlet weak var picture: UIImageView!
+    @IBOutlet weak var textOnLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
