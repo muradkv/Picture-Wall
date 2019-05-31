c203d8e346a69076b44543a4052db8432a632593
murad <muradkv@gmail.com>
2019-05-31 23:47:27 +0300

    Add class Picture

---
+++ Picture Wall/Picture.swift
@@ -0,0 +1,21 @@
+//
+//  Picture.swift
+//  Picture Wall
+//
+//  Created by murad on 31/05/2019.
+//  Copyright © 2019 murad. All rights reserved.
+//
+
+import Foundation
+
+class Picture {
+    
+    var pictureName: String
+    var image: String
+    
+    init(pictureName: String, image: String) {
+        self.pictureName = pictureName
+        self.image = image
+    }
+    
+}
