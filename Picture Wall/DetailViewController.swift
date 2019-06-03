b19a6007d2a2522f95d3d40eff4185dd68418457
murad <muradkv@gmail.com>
2019-06-03 23:42:57 +0300

    Add DetailViewController

---
+++ Picture Wall/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  Picture Wall
+//
+//  Created by murad on 03/06/2019.
+//  Copyright © 2019 murad. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
